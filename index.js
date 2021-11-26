378dbcff4b82ae86df6edf7fda4e4bc2a82a81a2
Sam Lynch <samdawkins1998@gmail.com>
2021-11-26 13:55:31 -0500

    commit tree 212d5127119657046c854d0f225655a6a4bf8a70

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+// function findMatching(drivers, string){
+//     for (const driver of drivers){
+//         if (string(driver)){
+//             console.log(drivers.name);
+//         }
+//     }
+// }
+// findMatching(drivers, function (drivers){
+//     return drivers.name === "Bobby";
+// })
+
+// function findMatching(){
+// const names = names.filter(name => name.includes('Bobby'));
+// }
+
+// console.log(result)
+
+function findMatching(drivers, string){
+    // const answer = drivers.filter(element => element === string || drivers.name.toLowerCase().includes(string.toLowerCase()))
+    let answer = drivers.filter(element => element === string || element === string.toLowerCase());
+    return answer ;
+}
+function fuzzyMatch(drivers, string){
+    let firstLetter = drivers.filter(driver => driver.slice(0, string.length) === string)
+    return firstLetter;
+}
+function matchName(drivers, string){
+    let name = drivers.filter(driver => driver.name == string);
+    return name;
+}
